0cdb63b335700d11395724514cd894e864946083
Your Name <reazulislam137@gmail.com>
2018-05-18 01:15:24 +0600

    first commit

---
+++ python turtle grapics.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.shape("turtle")
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(80)
+turtle.forward(90)
+turtle.left(80)
+turtle.forward(90)
+turtle.left(80)
+
+turtle.exitonclick()
+
